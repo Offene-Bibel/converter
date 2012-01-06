38a7e68524cde38faa674665fc7b51d3fd5233c5
Patrick Sebastian Zimmermann <patrick@zakweb.de>
2012-01-06 12:19:28 +0100

    Cleanup.

--- src/main/java/offeneBibel/osisExporter/Main.java
+++ src/main/java/offeneBibel/osisExporter/Main.java
@@ -1,11 +1,11 @@
 package offeneBibel.osisExporter;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.FileReader;
-import java.io.FileWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.Serializable;
 import java.io.StringReader;
 import java.io.UnsupportedEncodingException;
 import java.net.URL;
@@ -27,59 +27,67 @@ import org.parboiled.parserunners.TracingParseRunner;
 import org.parboiled.support.ParseTreeUtils;
 import org.parboiled.support.ParsingResult;
 import org.w3c.dom.Document;
-import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
 
 import offeneBibel.parser.ObAstNode;
 import offeneBibel.parser.OffeneBibelParser;
+import util.Misc;
 	
 public class Main
 {
 	//static final String m_urlBase = "http://www.offene-bibel.de/wiki/api.php?action=query&prop=revisions&rvprop=content&format=xml&titles=";
 	static final String m_urlBase = "http://www.offene-bibel.de/wiki/index.php5?action=raw&title=";
+	//the following list was created by combining the wiki page: Vorlage:Kapitelzahl and the OSIS 2.1.1 manual Appendix C.1
 	//static final String m_bibleBooks = "resources/bibleBooks.txt";
 	static final String m_bibleBooks = "resources/testbibleBooks.txt";
 	static final String m_studienFassungTemplate = "resources/offene-bibel-studienfassung_template.txt";
 	static final String m_leseFassungTemplate = "resources/offene-bibel-lesefassung_template.txt";
-	static final String m_studienFassungFilename = "offeneBibelStudienfassungModule.osis";
-	static final String m_leseFassungFilename = "offeneBibelLesefassungModule.osis";
+	static final String m_studienFassungFilename = "resources/offeneBibelStudienfassungModule.osis";
+	static final String m_leseFassungFilename = "resources/offeneBibelLesefassungModule.osis";
+	static final String m_bibleTextObjectFilename = "resources/bibleTextObjectFile.bin";
 	
 	public static void main(String [] args)
 	{
 		try {
-		List<Map<String, Object>> bibleTexts = retrieveBooks();
+		List<Map<String, Object>> bibleTexts = null;
+		File bibleTextObjectFile = new File(m_bibleTextObjectFilename);
+		if(bibleTextObjectFile.exists() == false) {
+			bibleTexts = retrieveBooks();
+			Misc.serializeBibleDataToFile((Serializable) bibleTexts, m_bibleTextObjectFilename);
+		}
+		else {
+			bibleTexts = (List<Map<String, Object>>) Misc.deserializeBibleDataToFile(m_bibleTextObjectFilename);
+		}
 		createOsisTextsForChapters(bibleTexts);
 		String studienFassung = constructOsisText(putOsisTextTogether(bibleTexts, false), false);
 		String leseFassung = constructOsisText(putOsisTextTogether(bibleTexts, true), true);
 		
-		writeFile(studienFassung, m_studienFassungFilename);
-		writeFile(leseFassung, m_leseFassungFilename);
+		Misc.writeFile(studienFassung, m_studienFassungFilename);
+		Misc.writeFile(leseFassung, m_leseFassungFilename);
 		
 		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
 		}
 	}
 
-	/*
-	def retrieveAndParseWikiPage(page):
-	    url = "http://www.offene-bibel.de/wiki/api.php?action=query&prop=revisions&rvprop=content&format=xml&titles=" + urllib.parse.quote(page)
-	    print(url)
-	    xml = urllib.request.urlopen(url).read().decode('utf-8')
-	    dom = parseString(xml)
-	    return dom
-	*/
 	private static String retrieveWikiPage(String wikiPage)
 	{
 		try {
 			URL url = new URL(m_urlBase + URLEncoder.encode(wikiPage, "UTF-8"));
 			System.out.println(url.toString());
 			BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream(), "UTF-8"));
-			String result = readBufferToString(in);
+			String result = Misc.readBufferToString(in);
 	        in.close();
 	        return result;
-        } catch (UnsupportedEncodingException e) {
+        } catch (FileNotFoundException e) {
+			// chapter not yet created, skip
+        	return null;
+		} 
+		catch (UnsupportedEncodingException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
 			e.printStackTrace();
@@ -87,59 +95,9 @@ public class Main
 		return null;
 	}
 	
-	private static String retrieveXmlWikiPage(String wikiPage)
-	{
-		try {
-			URL url = new URL(m_urlBase + URLEncoder.encode(wikiPage, "UTF-8"));
-			System.out.println(url.toString());
-			BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream(), "UTF-8"));
-			String xml = readBufferToString(in);
-	        in.close();
-	        return getContentFromXml(xml);
-        } catch (UnsupportedEncodingException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		return null;
-	}
-
-	/*
-	def getContentFromDom(dom):
-	    revElement = dom.getElementsByTagName("rev").item(0)
-	    if revElement is None:
-	        return ""
-	    else:
-	        return revElement.firstChild.data
-	*/
-	private static String getContentFromXml(String xml)
-	{
-		try {
-			DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-			DocumentBuilder db = dbf.newDocumentBuilder();
-			InputSource is = new InputSource(new StringReader(xml));
-			Document doc = db.parse(is);
-			doc.getDocumentElement().normalize();
-			NodeList nodeList = doc.getElementsByTagName("rev");
-			if(nodeList.getLength() == 0) {
-				return null;
-			}
-			else {
-				return nodeList.item(0).getFirstChild().getNodeValue();
-			}
-		} catch (ParserConfigurationException e1) {
-			e1.printStackTrace();
-		} catch (SAXException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		return null;
-	}
-
 	private static List<Map<String, Object>> retrieveBooks() throws IOException
 	{
-		List<List<String>> bookList = readCsv(m_bibleBooks);
+		List<List<String>> bookList = Misc.readCsv(m_bibleBooks);
 		List<Map<String, Object>>  bookDataCollection = new Vector<Map<String, Object>>();
 		for(List<String> book : bookList) {
 			Map<String, Object> bookData = new HashMap<String, Object>();
@@ -162,7 +120,7 @@ public class Main
 		
 		return bookDataCollection;
 	}
-	
+
 	/**
 	 * 4<-studienfassung, 5<-lesefassung
 	 * @param wikiTexts, 0 = german name, 1 = sword name, 2 = chapter count, 3 = wiki text
@@ -210,34 +168,6 @@ public class Main
 		}
 	}
 
-	/*
-	def writeOsisFile(templateFileName, outputFileName, content):
-	    
-	    templateFile = open(templateFileName)
-	    template = templateFile.read()
-	    templateFile.close()
-	    
-	    #TODO: get revision information from somewhere, possible?
-	    revisionInfo = "TODO"
-	    
-	    #template = template.replace("{{revision}}", revisionInfo, 1)
-	    template = template.replace("{{year}}", str(date.today().year), 1)
-	    template = template.replace("{{content}}", content, 1)
-	    
-	    outputFile = open(outputFileName, 'w', -1, 'utf_8', 'strict', '\n', True)
-	    outputFile.write(template)
-	    outputFile.close()
-	*/
-	private static String constructOsisText(String osisText, boolean leseFassung) throws IOException
-	{
-		String result = readFile(leseFassung ? m_leseFassungTemplate : m_studienFassungTemplate);
-		
-		result = result.replace("{{revision}}", "TODO");
-		result = result.replace("{{year}}", "" + Calendar.getInstance().get(Calendar.YEAR));
-		result = result.replace("{{content}}", osisText);
-		return result;
-	}
-	
 	private static String putOsisTextTogether(List<Map<String, Object>> bibleTexts, boolean leseFassung)
 	{
 		String result = "";
@@ -247,111 +177,66 @@ public class Main
 			result += "<div type=\"book\" osisID=\"" + bookData.get("swordName") + "\" canonical=\"true\">\n<title type=\"main\">" + bookData.get("germanName") + "</title>\n";
 			for(Map.Entry<Integer, String> chapterData : ((Map<Integer, String>)(bookData.get(studienVsLeseTag))).entrySet() ) {
 				result += "<chapter osisID=\"" + bookData.get("swordName") + "." + chapterData.getKey() + "\">\n<title type=\"chapter\">Kapitel " + chapterData.getKey() + "</title>\n";
-				result += chapterData.getKey();
+				result += chapterData.getValue();
 				result += "</chapter>\n";
 			}
 			result += "</div>\n";
 		}
 		return result;
 	}
-	
-	/*
-	def retrieveBooks():
-	    #bookRegex = re.compile('^\|(.+)=(\d+)$')
-	    #for bookLine in bookLines:
-	    #    result = bookRegex.match(bookLine)
-	    #    bookNamesWithChapterCount[result.group(1)] = int(result.group(2))
 
-	    # the following list was created by combining the wiki page: Vorlage:Kapitelzahl and the OSIS 2.1.1 manual Appendix C.1
-
-	    
-	    # this variable will be filled with the finished parsing results below
-	    result = ["", ""]
-
-	    #helper to add chapter markers
-	    def addChapterMarks(verseText, book, chapter):
-	        result = ""
-	        if verseText:
-	            chapterTag = book[1] + '.' + str(chapter)
-	            result += '<chapter osisID="' + chapterTag + '">\n<title type="chapter">Kapitel ' + str(chapter)  + '</title>\n'
-	            result += verseText
-	            result += '</chapter>\n'
-	        return result
-
-	    for biblePart in [otBooks, ntBooks, apBooks]:
-	#    for biblePart in [testBooks]:
-	        #TODO: part header goes here
-	        for book in biblePart:
-	            print("Parsing book " + book[0] + ".")
-	            #TODO: retrieve book description from book page
-	            
-	            bookBeginning = '<div type="book" osisID="' + book[1] + '" canonical="true">\n<title type="main">' + book[0] + '</title>\n'
-	            result[0] += bookBeginning
-	            result[1] += bookBeginning
-
-	            for chapter in range(1, book[2] + 1):
-	                bookDom = retrieveAndParseWikiPage(book[0] + " " + str(chapter)) # %20 = space in urls
-	                bookText = getContentFromDom(bookDom)
-	                verseTexts = parseBook(bookText, book[1], chapter)
-	                
-	                if len(verseTexts) == 2:
-	                    result[0] += addChapterMarks(verseTexts[0][0], book, chapter) # bibletext of the lesefassung
-	                    result[1] += addChapterMarks(verseTexts[1][0], book, chapter) # bibletext of the studienfassung
-
-	            bookEnd = '</div>\n'
-	            result[0] += bookEnd
-	            result[1] += bookEnd
-
-	            print("Done.")
-	    return result
-	*/
-	    
-	/*
-		# program starts here
-		#TODO: insert date in fromat: yyyy.mm.ddThh.mm.ss
-	
-		content = retrieveBooks()
-		writeOsisFile("offene-bibel-lesefassung_template.txt", "offeneBibelLesefassungModule.osis", content[0])
-		writeOsisFile("offene-bibel-studienfassung_template.txt", "offeneBibelStudienfassungModule.osis", content[1])
-	*/
-	
-	
-	
-	private static void writeFile(String text, String filename) throws IOException
+	private static String constructOsisText(String osisText, boolean leseFassung) throws IOException
 	{
-		BufferedWriter out = new BufferedWriter(new FileWriter(filename));
-		out.write(text);
-		out.close();
-	}
-
-	private static String readFile(String file) throws IOException
-	{
-		BufferedReader reader = new BufferedReader(new FileReader(file));
-		return readBufferToString(reader);
-	}
-	
-	private static String readBufferToString(BufferedReader reader) throws IOException
-	{
-		String result = "";
-		char[] cbuf = new char[512];
-		int readCount;
-		while((readCount = reader.read(cbuf)) >= 0)
-			result += String.valueOf(cbuf, 0, readCount);
+		String result = Misc.readFile(leseFassung ? m_leseFassungTemplate : m_studienFassungTemplate);
+		
+		result = result.replace("{{revision}}", "TODO");
+		result = result.replace("{{year}}", "" + Calendar.getInstance().get(Calendar.YEAR));
+		result = result.replace("{{content}}", osisText);
 		return result;
 	}
-	
-	private static List<List<String>> readCsv(String filename) throws IOException
+
+	@SuppressWarnings("unused")
+	@Deprecated
+	private static String retrieveXmlWikiPage(String wikiPage)
 	{
-		List<List<String>> result = new Vector<List<String>>();
-		BufferedReader stream = new BufferedReader(new FileReader(filename));
-		String line;
-		while((line = stream.readLine()) != null) {
-			List<String> lineElements = new Vector<String>();
-			for(String element : line.split(",")) {
-				lineElements.add(element.trim());
-			}
-			result.add(lineElements);
+		try {
+			URL url = new URL(m_urlBase + URLEncoder.encode(wikiPage, "UTF-8"));
+			System.out.println(url.toString());
+			BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream(), "UTF-8"));
+			String xml = Misc.readBufferToString(in);
+	        in.close();
+	        return getContentFromXml(xml);
+        } catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
-		return result;
+		return null;
+	}
+
+	@Deprecated
+	private static String getContentFromXml(String xml)
+	{
+		try {
+			DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+			DocumentBuilder db = dbf.newDocumentBuilder();
+			InputSource is = new InputSource(new StringReader(xml));
+			Document doc = db.parse(is);
+			doc.getDocumentElement().normalize();
+			NodeList nodeList = doc.getElementsByTagName("rev");
+			if(nodeList.getLength() == 0) {
+				return null;
+			}
+			else {
+				return nodeList.item(0).getFirstChild().getNodeValue();
+			}
+		} catch (ParserConfigurationException e1) {
+			e1.printStackTrace();
+		} catch (SAXException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return null;
 	}
 }
---
+++ src/main/java/util/Misc.java
@@ -0,0 +1,74 @@
+package util;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+import java.util.List;
+import java.util.Vector;
+
+public class Misc {
+
+	public static List<List<String>> readCsv(String filename) throws IOException
+	{
+		List<List<String>> result = new Vector<List<String>>();
+		BufferedReader stream = new BufferedReader(new FileReader(filename));
+		String line;
+		while((line = stream.readLine()) != null) {
+			List<String> lineElements = new Vector<String>();
+			for(String element : line.split(",")) {
+				lineElements.add(element.trim());
+			}
+			result.add(lineElements);
+		}
+		return result;
+	}
+
+	public static String readBufferToString(BufferedReader reader) throws IOException
+	{
+		String result = "";
+		char[] cbuf = new char[512];
+		int readCount;
+		while((readCount = reader.read(cbuf)) >= 0)
+			result += String.valueOf(cbuf, 0, readCount);
+		return result;
+	}
+
+	public static void writeFile(String text, String filename) throws IOException
+	{
+		BufferedWriter out = new BufferedWriter(new FileWriter(filename));
+		out.write(text);
+		out.close();
+	}
+
+	public static String readFile(String file) throws IOException
+	{
+		BufferedReader reader = new BufferedReader(new FileReader(file));
+		return readBufferToString(reader);
+	}
+
+	public static void serializeBibleDataToFile(Serializable data, String filename) throws IOException
+	{
+	       FileOutputStream fileStream = new FileOutputStream(filename);
+	       ObjectOutputStream objectStream = new ObjectOutputStream(fileStream);
+	       objectStream.writeObject(data);
+	       objectStream.flush();
+	       fileStream.close();
+	
+	}
+
+	public static Object deserializeBibleDataToFile(String filename) throws IOException, ClassNotFoundException
+	{
+	       FileInputStream fileStream = new FileInputStream(filename);
+	       ObjectInputStream objectStream = new ObjectInputStream(fileStream);
+	       Object data = objectStream.readObject();
+	       return data;
+	}
+
+}
