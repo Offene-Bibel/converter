4e2883d721b9ba37a4b5caf975c4b464a64df276
Patrick Sebastian Zimmermann <patrick@zakweb.de>
2012-05-31 21:58:52 +0200

    Fix sword converter, it accessed wrong resources dir.
    Signed-off-by: Patrick Sebastian Zimmermann <patrick@zakweb.de>

--- resources/bin/swordConverter.sh
+++ resources/bin/swordConverter.sh
@@ -2,6 +2,7 @@
 cd ../tmp
 
 resources="../resources"
+results="../results"
 rm -r offbileModule &> /dev/null
 
 mkdir offbileModule
@@ -15,7 +16,7 @@ cp ${resources}/offbile.conf offbileModule/mods.d
 
 #xmllint --noout --schema http://www.bibletechnologies.net/osisCore.2.1.1.xsd offeneBibelModule.osis
 #~/Documents/sword/svn/sword/utilities/osis2mod offbileModule/modules/texts/rawtext/offbile/ offeneBibelLesefassungModule.osis -d 8
-~/Documents/sword/svn/sword/utilities/osis2mod offbileModule/modules/texts/ztext/offbile/ ${resources}/offeneBibelLesefassungModule.osis -z
+~/Documents/sword/svn/sword/utilities/osis2mod offbileModule/modules/texts/ztext/offbile/ ${results}/offeneBibelLesefassungModule.osis -z
 
 cp -r offbileModule/* ~/.sword
 
@@ -32,7 +33,7 @@ cp ${resources}/offbist.conf offbistModule/mods.d
 
 #xmllint --noout --schema http://www.bibletechnologies.net/osisCore.2.1.1.xsd offeneBibelModule.osis
 #~/Documents/sword/svn/sword/utilities/osis2mod offbistModule/modules/texts/rawtext/offbist/ offeneBibelStudienfassungModule.osis -d 8
-~/Documents/sword/svn/sword/utilities/osis2mod offbistModule/modules/texts/ztext/offbist/ ${resources}/offeneBibelStudienfassungModule.osis -z
+~/Documents/sword/svn/sword/utilities/osis2mod offbistModule/modules/texts/ztext/offbist/ ${results}/offeneBibelStudienfassungModule.osis -z
 
 cp -r offbistModule/* ~/.sword
 
