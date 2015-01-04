5352f8038ab487080804c31b24d30cb7b81f2bd7
Michael Schierl <schierlm@gmx.de>
2015-01-04 16:55:18 +0100

    Fix file encoding for Windows
    On Unix, if the default locale is a UTF-8 locale, files will be stored as
    UTF-8 if no charset is given. Windows defaults to the old codepage
    charsets, and none of them contains Hebrew, Greek *and* Latin characters.
    Therefore also use UTF-8 on Windows.

--- resources/bin/exporter.bat
+++ resources/bin/exporter.bat
@@ -1,3 +1,4 @@
 cd ..
-java -cp lib/Parser-0.0.1-SNAPSHOT.jar offeneBibel.osisExporter.Exporter %*
+java -Dfile.encoding=UTF-8 -cp lib/Parser-0.0.1-SNAPSHOT.jar offeneBibel.osisExporter.Exporter %*
 cd bin
+
