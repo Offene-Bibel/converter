a05203735476b0721a8c699289c8be7cc38f3a05
Patrick Sebastian Zimmermann <patrick@zakweb.de>
2015-02-10 22:48:44 +0100

    Allow running build.sh script from other folders.

--- build.sh
+++ build.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
+
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+
+pushd $DIR
 mvn package
 mkdir install 2>/dev/null
 cp -r resources/* install
 cp -r target/lib install
 cp target/Parser-0.0.1-SNAPSHOT.jar install/lib
+popd
+
